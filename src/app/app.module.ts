985c649f5f62d062ec094e111c0757a8cfb24896
Yohei Munesada <y.munesada@gmail.com>
2018-02-07 15:04:36 +0900

    Add routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+
+const appRoutes: Routes = [
+  { path : 'home', component : HomeComponent },
+  { path : '', redirectTo : '/home', pathMatch : 'full' }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
